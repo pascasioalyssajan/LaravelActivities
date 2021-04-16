b897124f10fa0e6aa5a9e4a9215fa53fd311ce35
pascasioalyssajan <201811114@gordoncollege.edu.ph>
2021-04-16 11:39:10 +0800

    add crud

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Auth\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/posts', 'PostController');
 
 Auth::routes();
 
